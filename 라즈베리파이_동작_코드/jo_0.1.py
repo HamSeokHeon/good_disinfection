c7c98544bcc1d24e56c15f409b0d593fe9415744
SeoKeon <saekikazumi@naver.com>
2020-07-26 20:41:34 +0900

    seokheon

--- 라즈베리파이_동작_코드/jo_0.1.py
+++ 라즈베리파이_동작_코드/jo_0.1.py
@@ -17,7 +17,7 @@ ser = serial.Serial('/dev/ttyACM0',9600)
 #        print("aaaaaaaaaaaaaaaa")
 #333333333################################3
 cap= cv2.VideoCapture(-1)
-body_cascade = cv2.CascadeClassifier('/haarcascade_eye.xml')
+body_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
 
 #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
 #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
