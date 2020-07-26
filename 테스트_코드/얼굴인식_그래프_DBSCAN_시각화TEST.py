95be06ad8189834d3e1fa8070825a04822359d01
SeoKeon <saekikazumi@naver.com>
2020-07-26 20:00:01 +0900

    200726

--- 테스트_코드/얼굴인식_그래프_DBSCAN_시각화TEST.py
+++ 테스트_코드/얼굴인식_그래프_DBSCAN_시각화TEST.py
@@ -17,14 +17,14 @@ cap= cv2.VideoCapture(0)
 body_cascade = cv2.CascadeClassifier('../haarcascade_frontalface_default.xml')
 
 #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    #cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 #if cap.isOpened()==False:
  #   exit()
 
 face_points = np.empty((0,2) , int)
 temp_W_H = []
  
-i = 0;
+i = 0
 
 
 while True:
